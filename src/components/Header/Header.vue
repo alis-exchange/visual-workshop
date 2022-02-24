e4bed55e65437e2c0c5a6072969742790bfc16cd
Madelein <madelein@rezco.co.za>
2022-02-24 11:47:37 +0200

    chore(main): rather use Visuals

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -11,7 +11,7 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
       <v-toolbar-title>
-        Visual Workshop
+        Visuals Workshop
       </v-toolbar-title>
 
       <v-spacer />
